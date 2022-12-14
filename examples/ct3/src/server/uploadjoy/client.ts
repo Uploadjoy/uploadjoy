0147296bc107354f15c7557dd63bbef37458776c
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-12-13 20:32:00 -0500

    comment out env vars to pass lint

--- examples/ct3/src/server/uploadjoy/client.ts
+++ examples/ct3/src/server/uploadjoy/client.ts
@@ -1,6 +1,5 @@
 import { Uploadjoy } from "@uploadjoy/client";
-import { env } from "../../env/server.mjs";
 
 export const ujClient = new Uploadjoy({
-  apiToken: env.UPLOADJOY_TOKEN,
+  apiToken: "abcdefg",
 });
