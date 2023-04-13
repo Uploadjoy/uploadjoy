3db5006a1c1de4d9addad7db867dc9197a22d913
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2023-04-13 18:07:14 -0400

    add uploader common

--- examples/create-t3-app/src/pages/index.tsx
+++ examples/create-t3-app/src/pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
     <>
       <main className="dark h-full bg-zinc-900 text-zinc-50 p-10">
         <h1 className="text-xl font-semibold text-center mb-2">
-          Uploadjoy create-t3-app dropzone examples with react-dropzone
+          Uploadjoy create-t3-app dropzone examples with react-dropzone + axios
         </h1>
         <p className="text-center mb-8">
           Open DevTools to see logs and learn how these components work
---
+++ packages/uploader-common/src/externalApi/apiCall.ts
@@ -0,0 +1,81 @@
+import {
+  externalApiPutObjectApiOutputSchema,
+  ExternalApiPutObjectApiOutput,
+  ApiCallArgs,
+} from "../validators";
+
+export class FetchPresignedUrlsError extends Error {
+  status: number;
+  body: any;
+
+  constructor(message: string, response: Response) {
+    super(message);
+    this.stack = new Error().stack;
+
+    this.name = this.constructor.name;
+    this.status = response.status;
+    this.body = response.json();
+  }
+
+  statusCode() {
+    return this.status;
+  }
+
+  async responseBody() {
+    return await this.body;
+  }
+}
+
+const ENDPOINTS = {
+  uploadObjects: `/presigned-url/put-objects`,
+} as const;
+
+const getEndpointUrl = (baseUrl: string, endpoint: keyof typeof ENDPOINTS) => {
+  return `${baseUrl}${ENDPOINTS[endpoint]}`;
+};
+
+const createAuthorizationHeader = (token: string) => {
+  return `Bearer ${token}`;
+};
+
+export const fetchPresignedUrlsFromExternalApi = async ({
+  token,
+  input,
+  customApiUrl,
+}: ApiCallArgs) => {
+  const authHeader = createAuthorizationHeader(token);
+  const response = await fetch(getEndpointUrl(customApiUrl, "uploadObjects"), {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: authHeader,
+    },
+    body: JSON.stringify(input),
+    cache: "no-store",
+  });
+
+  if (!response.ok) {
+    throw new FetchPresignedUrlsError(
+      "Failed to fetch presigned URLs",
+      response,
+    );
+  }
+
+  const data = await response.json();
+
+  const responseParseResult =
+    externalApiPutObjectApiOutputSchema.safeParse(data);
+  if (!responseParseResult.success) {
+    // This should never happen, but if it does, we want to know about it
+    console.error(
+      "Failed to parse response from Uploadjoy API. This is a bug.",
+    );
+    console.error(responseParseResult.error.issues);
+    throw new FetchPresignedUrlsError(
+      "Failed to fetch presigned URLs",
+      response,
+    );
+  }
+
+  return data as ExternalApiPutObjectApiOutput;
+};
