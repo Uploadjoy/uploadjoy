025c7ad977ddd2d1492c46377367ff6826cf9053
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2023-04-15 09:00:00 -0400

    new t3 example

--- examples/create-t3-app/src/server/uploadjoy/client.ts
+++
@@ -1,5 +0,0 @@
-import { Uploadjoy } from "@uploadjoy/client";
-
-export const ujClient = new Uploadjoy({
-  apiToken: process.env.UPLOADJOY_TOKEN as string,
-});
--- examples/create-t3-app/tailwind.config.ts
+++ examples/create-t3-app/tailwind.config.ts
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { type Config } from "tailwindcss";
+
+export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
