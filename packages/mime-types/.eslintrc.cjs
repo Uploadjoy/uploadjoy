67946339fa7809a9807d47b832809a6deb1f55a7
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2023-06-13 22:26:33 -0400

    fix lint

---
+++ packages/mime-types/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  plugins: ["unicorn", "turbo"],
+  extends: [
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: "latest", // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of import
+    project: "./tsconfig.json", // Allows for the use of rules which require parserServices to be generated
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/ban-types": "warn",
+    "unicorn/filename-case": [
+      "error",
+      {
+        cases: {
+          camelCase: true,
+          pascalCase: true,
+          kebabCase: true,
+        },
+      },
+    ],
+  },
+};
